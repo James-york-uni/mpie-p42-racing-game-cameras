5b7c856a30d6c65c431b5a8c1f6f845f1b7dbb73
James-york-uni <zzw518@york.ac.uk>
2025-10-16 17:54:28 +0100

    2 player

--- Assets/CameraFollow2.cs
+++ Assets/CameraFollow2.cs
@@ -1,21 +1,21 @@
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class CameraFollow2 : MonoBehaviour
 {
     Vector3 offset;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GameObject target = GameObject.Find ("Car");
+        GameObject target = GameObject.Find ("Car 2");
         offset = transform.position - target.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        GameObject target = GameObject.Find ("Car");
+        GameObject target = GameObject.Find ("Car 2");
 
-        Vector3 newPos = target.transform.position + offset;
-        transform.position = newPos;
+        Vector3 newPos2 = target.transform.position + offset;
+        transform.position = newPos2;
     }
 }
